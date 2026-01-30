4ff23d9698d6f3f2a7857e88f192c77c819ef291
richeju <91011913+richeju@users.noreply.github.com>
2026-01-30 17:06:56 +0100

    Add Dock configuration script

---
+++ run_once_configure-dock-darwin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo "🎨 Configuring Dock..."
+
+defaults write com.apple.dock tilesize -int 48
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.5
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock mineffect -string "scale"
+defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock show-process-indicators -bool true
+
+killall Dock
+
+echo "✅ Dock configured!"
